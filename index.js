f3d103d5f354bde64066d54eb0ae089c22d44288
Henrique Cervi Cutri <henrique.cutri@ab-inbev.com>
2025-04-06 12:57:33 -0300

    Here we go! Starting the JavaScript practice repo

---
+++ index.js
@@ -0,0 +1,85 @@
+/**
+ * Console API
+ */
+
+console.log("Aula Inicial JS.")
+console.error("Falha na execução.")
+console.warn("Atenção.")
+console.table(    [
+{
+    nome: 'Henrique',
+    turma: '02',
+    disciplina: 'Prog JS'
+},
+{
+    nome: 'Goku',
+    turma: '02',
+    disciplina: 'Prog JS'
+}
+])
+
+
+// informações que não alteram utilizamos const
+const nome = 'Marley'
+const raca = 'Australian Shepherd'
+const sexo = 'Macho'
+const cor = 'Branco e Preto'
+const dataDeNascimento = '01/12/2023'
+const porte = 'Médio'
+
+// informações que alteram utilizamos let/var
+let idade = 2
+let peso = 20
+let vacinado = true
+let castrado = false
+let tamanho = 'Médio'
+
+
+
+console.log("Aula incial")
+const pet = 
+[
+    {nome: 'Lure', idade: '2 anos'}
+]
+console.table(pet)
+
+
+
+const numeroAula = "03"
+const turma = "02"
+let data = "05 de Abril"
+
+console.log("Aula 03 da Turma " + turma + " no Sábado dia 05 de Abril")
+
+console.log(`Aula ${ numeroAula } da Turma ${ turma } no Sábado dia ${ data }`)
+
+console.log(data.length)
+
+// split - separar a string
+const nomesDeAlunos = "Giuliana André Goku Lucas Lenilson"
+
+const nomesDeAlunosSplit = nomesDeAlunos.split(" ")
+
+console.log(nomesDeAlunosSplit)
+
+console.log(nomesDeAlunos.toLowerCase())  // minúsculo
+console.log(nomesDeAlunos.toUpperCase())  // maiúsculo
+
+console.log(nomesDeAlunos.includes("G"))
+
+// Chai - biblioteca de asserções
+
+console.log(nomesDeAlunos.replaceAll('a', 'i'))
+
+const conceitosLogica = "            inteiro real cadeia de caracteres...           "
+
+console.log(conceitosLogica.trim())
+
+let dataParaCortar = "05 de Abril"
+const cpf = "86189924611"
+
+console.log(dataParaCortar.slice(0, 2))
+console.log(cpf.slice(0, 3))
+
+// 05 de Abril
+// 0123456789
---
+++ tag.js
@@ -0,0 +1,30 @@
+
+let nome =" Marley";
+let idade = 2;
+let peso = 20;
+let raca = "Australian Shepherd";
+let adotoado = false;
+
+nome.toUpperCase()
+racaAtualizada = raca.charAt(0).toUpperCase() + raca.slice(1).toLowerCase()
+
+console.log(`Tag do dog: ${nome.toUpperCase()}`)
+console.log(`Raça do dog: ${racaAtualizada}`)
+console.log(`Peso do dog: ${peso}`)
+
+console.log(`🏷️\nTag: ${nome}, ${racaAtualizada}, ${peso}` )
+
+
+let nomePet = "Adivinha Nome",
+racaPet = "sem raça",
+idadePet = "3 meses",
+pesoPet = "1.2kg",
+adotadoPet = "Sim";
+petNome = nomePet.toUpperCase()
+racaPetAtualizada = racaPet.charAt(0).toUpperCase() + racaPet.slice(1).toLowerCase()
+animal = [{
+    nomePet: `${petNome} ❤️`, 
+    pesoPet: pesoPet,     
+    racaPetAtualizada: racaPetAtualizada
+}]
+console.log(animal)
