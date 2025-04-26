499db76a2aed54f93bd99582a6ed467f94001e4b
Henrique Cervi Cutri <henrique.cutri@ab-inbev.com>
2025-04-26 10:38:39 -0300

    Learning JavaScript concepts-using function

--- conceitos/atividade3.js
+++ conceitos/atividade3.js
@@ -17,10 +17,26 @@ const idadeMinima = 2
 const dog = {
     nome: 'Marley',
     idade: 1,
-    porte: 'pequeno'
+    porte: 'médio'
 }
+
+const nome = "Nome Dog"
+const idade = 0
+const porte = 'pequeno'
 
 const adotar = dog.porte === 'pequeno' || dog.idade >= idadeMinima ? 'pode ser adotado' : 'não pode ser adotado'
 
 console.log(`O dog ${dog.nome} de ${dog.idade} ano(s) e porte ${dog.porte}, ${adotar}`)
-            
+
+function verificarSePodeSerAdotado(idade, porte) {
+    const adotar = porte === 'pequeno' 
+        || idade >= idadeMinima ? 'pode ser adotado' 
+        : 'não pode ser adotado'
+
+    console.log(`O dog ${nome} de ${idade} ano(s) e porte ${porte}, ${adotar}`)    
+}
+
+verificarSePodeSerAdotado(1, 'pequeno')
+verificarSePodeSerAdotado(2, 'médio')
+verificarSePodeSerAdotado(3, 'grande')
+verificarSePodeSerAdotado(1, 'médio')            
