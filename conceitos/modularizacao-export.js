19752a139c0c5568548de3077dbcd3afa8196c9f
Henrique Cervi Cutri <henrique.cutri@ab-inbev.com>
2025-05-10 14:01:36 -0300

    starting with modules

---
+++ conceitos/modularizacao-export.js
@@ -0,0 +1,18 @@
+/*
+    Sintaxe CommonJs:
+        module.exports = {} / require('') 
+
+    ESM - Ecmascript Standard Modules
+        export {} / import
+
+*/
+
+function exibirNomePet(nomePet) {
+    console.log(`O nome do pet é: ${nomePet}`)
+}
+
+exibirNomePet('Export')
+
+export {
+    exibirNomePet
+}
