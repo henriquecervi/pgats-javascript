50ac9bb347c1aba66e45e76cad3189534414e0cf
Henrique Cervi Cutri <henrique.cutri@ab-inbev.com>
2025-05-10 17:12:23 -0300

    async and await

---
+++ conceitos/async-await.js
@@ -0,0 +1,19 @@
+
+/*
+    quando utilizado o async, o retorno é uma promise
+    async = assíncrono
+
+    sempre que uma função for async, precisamos utilizar o await, senão teremos a promise invés do resultado.
+*/
+
+async function exibirNomeFormatado(nome) {
+    return nome.toUpperCase()
+}
+
+console.log(exibirNomeFormatado('Mimosa'))
+
+async function exibirNomeFormatadoMinusculo(nome) {
+    return nome.toLowerCase()
+}
+
+console.log(await exibirNomeFormatadoMinusculo('Mimosa'))
